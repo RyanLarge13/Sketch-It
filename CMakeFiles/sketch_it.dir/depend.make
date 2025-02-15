081a74022946283f5de48524b063f47f3feded51
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2025-02-15 09:57:03 -0500

    Untracking Files And Dirs

--- CMakeFiles/sketch_it.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sketch_it.
-# This may be replaced when dependencies are built.
--- CMakeFiles/sketch_it.dir/src/Files/UserData.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sketch_it.dir/src/Files/UserData.cpp.o: \
- /home/ryan/Coding/C++/Sketch-It/src/Files/UserData.cpp \
- /usr/include/stdc-predef.h
